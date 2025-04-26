e2dcdf9feb37429374540805de8fe5d14dda85b0
Yayo_joaco <gjrabanal@gmail.com>
2025-04-25 20:32:43 -0500

    first commit

---
+++ src/main/java/com/example/laboratorio320221957/Laboratorio320221957Application.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio320221957;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio320221957Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio320221957Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/laboratorio320221957/Laboratorio320221957ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorio320221957;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio320221957ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
